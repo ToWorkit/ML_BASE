6981cf3606888371439e571dd9971636e5452f10
ToWorkit <1211126374@qq.com>
2017-10-13 12:49:06 +0800

    20171013

--- Reptile/bs4/lxml/lxml_01.py
+++ Reptile/bs4/lxml/lxml_01.py
@@ -1,3 +1,15 @@
 import bs4
 soup = bs4.BeautifulSoup(open('../one.html'),'lxml')
-print(soup.prettify())
+# print(soup.prettify())
+
+a_tag = soup.find_all('a')
+
+print(a_tag[0])
+print(a_tag[0].contents)
+
+title_tag = soup.head.contents[0]
+print(title_tag.contents)
+for child in title_tag.children:
+	print(child)
+
+# soup.find_all('a')	
