944b40d98c6d96a0607a3208e4cdc9d0d4b94393
Doing <helong@yuelu.net>
2017-10-19 10:53:32 +0800

    20171019

---
+++ Reptile/Selenium/readme.py
@@ -0,0 +1,10 @@
+# 在写Python爬虫的时候，最麻烦的不是那些海量的静态网站，而是那些通过JavaScript获取数据的站点。Python本身对js的支持就不好，所以就有良心的开发者来做贡献了，这就是Selenium，他本身可以模拟真实的浏览器，浏览器所具有的功能他一个都不拉下，加载js更是小菜了
+# https://zhuanlan.zhihu.com/p/27115580
+# C:\Users\hedy\AppData\Local\Programs\Python\Python36\Scripts\;C:\Users\hedy\AppData\Local\Programs\Python\Python36\
+
+# pip 换源
+# http://blog.csdn.net/lambert310/article/details/52412059
+
+# 安装全家桶(ipython,jupyter notebook)
+# https://jingyan.baidu.com/article/cbcede070c8eac02f40b4d8e.html
+# http://blog.csdn.net/sanshixia/article/details/53996126
