dc33edc4ba9106bc0646c0929bb8a45cfcb1f7ee
Toworkit <1211126374@qq.com>
2017-10-21 02:01:46 +0800

    20171021

--- Numpy/zeros.py
+++ Numpy/zeros.py
@@ -3,3 +3,10 @@ from numpy import zeros, ones
 print(zeros((3, 2)))
 # 全一
 print(ones((3, 2)))
+import tensorflow as tf
+_x = tf.Variable(tf.zeros([2, 3]))
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+  sess.run(init)
+  result = sess.run(_x)
+  print(result)
